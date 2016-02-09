b3bc83fb26237f1c2256be373b72871cec809fff
Laurent Duchesne <l@urent.org>
2016-02-09 10:56:53 -0500

    Unused files.

--- approx_pi.h
+++
@@ -1,1 +0,0 @@
-double approx_pi(int intervals);
--- approx_pi_cython4.pyx
+++
@@ -1,5 +0,0 @@
-cdef extern from "approx_pi.h":
-    double c_approx_pi "approx_pi" (int intervals)
-
-def approx_pi(int intervals):
-    return c_approx_pi(intervals)
