6eeb7378ee64020bd8f5085c1834f48dd9966f8c
Laurent Duchesne <l@urent.org>
2016-02-10 19:55:00 -0500

    Removed unused files.

--- setup_approx_pi_swig.py
+++
@@ -1,5 +0,0 @@
-from distutils.core import setup, Extension
-# define the extension module
-module = Extension('_approx_pi_swig', sources=['approx_pi.c', 'approx_pi_swig.i'])
-# run the setup
-setup(ext_modules=[module])
